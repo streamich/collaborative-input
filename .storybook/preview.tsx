ca34dbaa0725895634ede9da016c1c3f805f2a40
streamich <streamich@gmail.com>
2023-12-01 02:07:43 +0100

    feat: 🎸 implment StrBinding

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,9 +1,4 @@
-import * as React from 'react';
 import {Preview} from '@storybook/react';
-import {Provider, GlobalCss} from 'nano-theme';
-import {useStoryContext} from '@storybook/addons';
-
-import 'nano-theme/lib/global-reset';
 
 const preview: Preview = {
   parameters: {
@@ -14,19 +9,7 @@ const preview: Preview = {
         date: /Date$/i,
       },
     },
-  },
-  decorators: [
-    (Story) => {
-      const color = useStoryContext()?.globals?.backgrounds?.value;
-      const isDark = color ? String(color)[1].toLowerCase() !== 'f' : false;
-      return (
-        <Provider theme={isDark ? 'dark' : 'light'}>
-          <GlobalCss />
-          <Story />
-        </Provider>
-      );
-    },
-  ],
+  }
 };
 
 export default preview;
